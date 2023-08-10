93d84ffaac22d8dc32e3f52d7f5270d6572ee5c9
Larissa Andrade <larissa.sandrade@hotmail.com>
2023-08-10 14:45:21 -0300

    feat: criar model, controller e repository de tutor

---
+++ src/main/java/com/adopet/api/controllers/TutorController.java
@@ -0,0 +1,7 @@
+package com.adopet.api.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class TutorController {
+}
---
+++ src/main/java/com/adopet/api/repositories/TutorRepository.java
@@ -0,0 +1,7 @@
+package com.adopet.api.repositories;
+
+import com.adopet.api.models.Tutor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TutorRepository extends JpaRepository<Tutor, Integer> {
+}

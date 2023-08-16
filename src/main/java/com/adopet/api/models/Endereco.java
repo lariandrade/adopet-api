7b1989330405a6a5898a0cc204428562271ebbb6
Larissa Andrade <larissa.sandrade@hotmail.com>
2023-08-16 14:46:21 -0300

    feat: criar método para atualizar dados do endereço

--- src/main/java/com/adopet/api/models/Endereco.java
+++ src/main/java/com/adopet/api/models/Endereco.java
@@ -1,5 +1,6 @@
 package com.adopet.api.models;
 
+import com.adopet.api.dtos.DadosEnderecoDTO;
 import jakarta.persistence.Embeddable;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,4 +11,21 @@ import lombok.NoArgsConstructor;
 public class Endereco {
     private String cidade;
     private String UF;
+
+    public Endereco(DadosEnderecoDTO endereco) {
+        this.cidade = endereco.cidade();
+        this.UF = endereco.UF();
+    }
+
+    public void atualizarInformacoes(DadosEnderecoDTO dados) {
+
+        if (dados.cidade() != null){
+            this.cidade = dados.cidade();
+        }
+
+        if (dados.UF() != null){
+            this.UF = dados.UF();
+        }
+
+    }
 }
